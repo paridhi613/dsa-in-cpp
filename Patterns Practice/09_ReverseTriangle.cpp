a3c45e4ec91c12656d3b13019e7155da850e1d09
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-07 23:24:52 +0530

    comment added

--- Patterns Practice/09_ReverseTriangle.cpp
+++ Patterns Practice/09_ReverseTriangle.cpp
@@ -19,7 +19,7 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = i+1; j > 0; j--)
+        for (int j = i+1; j > 0; j--)    //number prints in the reverse order
         {
             cout << j << " ";
         }
