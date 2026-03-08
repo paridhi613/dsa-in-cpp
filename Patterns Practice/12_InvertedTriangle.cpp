06fbee6909db834e0cb026b6ff0a49bd260d0137
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-08 15:44:34 +0530

    comments added

--- Patterns Practice/12_InvertedTriangle.cpp
+++ Patterns Practice/12_InvertedTriangle.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+/*
+      OUTPUT:
+       1111
+        222
+         33
+          4
+
+      for n=4   
+*/
+
 int main()
 {
     int n;
@@ -9,11 +19,11 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < i; j++)      //for spaces in the start
         {
             cout << " ";
         }
-        for (int j = 0; j < n - i; j++)
+        for (int j = 0; j < n - i; j++)   //for printing the numbers
         {
             cout << i + 1;
         }
