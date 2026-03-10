65cb6f71293173ff09feb80ce6869dcc77847dc4
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-10 23:09:16 +0530

    parameters in function

---
+++ 04_Functions/03_Parameters.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//function for sum
+int sum(int a, int b)  //defining the parameters
+{
+    int s = a + b;
+    return s;
+}
+
+int main()
+{
+    //function call
+    cout << sum(45, 90)<<endl;  //calling the parameters
+    cout << sum(5, 9)<<endl;
+}
