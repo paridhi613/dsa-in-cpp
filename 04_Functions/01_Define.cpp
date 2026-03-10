68de81f0239d7eba3af46b7b46a92201fe65d4f7
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-10 21:24:47 +0530

    functions started

---
+++ 04_Functions/01_Define.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+//function definition
+void printHello()
+{
+    cout << "hello!";
+}
+
+int main()
+{
+    //function call/invoke
+    printHello();
+}
