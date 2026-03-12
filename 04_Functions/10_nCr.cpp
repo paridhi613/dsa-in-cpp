1f7da172655d5327cdee918842bfaeeab25327b4
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-12 20:24:11 +0530

    comments added

--- 04_Functions/10_nCr.cpp
+++ 04_Functions/10_nCr.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 using namespace std;
 
+
+//function to find the nCr value
 int nCr(int n, int r)
 {
     int fact = 1;
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++)    //n!
     {
         fact *= i;
     }
 
     int fact2 = 1;
-    for (int i = 1; i <= r; i++)
+    for (int i = 1; i <= r; i++)  //r!
     {
         fact2 *= i;
     }
 
     int fact3 = 1;
-    for (int i = 1; i <= (n - r); i++)
+    for (int i = 1; i <= (n - r); i++)  //n-r!
     {
         fact3 *= i;
     }
 
-    return fact / (fact2 * fact3);
+    return fact / (fact2 * fact3);  //n!/r!(n-r)!
 }
 
 int main()
