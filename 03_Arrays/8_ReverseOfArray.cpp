a4069a7ba978e646b53fbe57b42e4648ca60b41b
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-15 22:51:12 +0530

    comments added

--- 03_Arrays/8_ReverseOfArray.cpp
+++ 03_Arrays/8_ReverseOfArray.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
+//used the 2 pointer method to reverse the array
 void reverseArray(int arr[], int size)
 {
-    int start = 0, end = size - 1;
+    int start = 0, end = size - 1;       //the starting and the ending point of the array
 
-    while (start < end)
+    while (start < end)                  //will swap the values only till start is less than end value
     {
-        swap(arr[start], arr[end]);
-        start++;
-        end--;
+        swap(arr[start], arr[end]);      //swaping the start and end value
+        start++;                         //from start the value increases
+        end--;                           //frim end the value decreases
     }
 }
 
@@ -18,10 +19,10 @@ int main()
     int arr[] = {4, 2, 7, 8, 1, 2, 5};
     int size = 7;
 
-    reverseArray(arr, size);
+    reverseArray(arr, size);              //calling the reverse function
 
     for (int i = 0; i < size; i++)
     {
-        cout << arr[i] << " ";
+        cout << arr[i] << " ";            //prints the reversed values
     }
 }
